2ded13d3a8b334e6948f0bf8bd1832e26d54edbf
Wojciech Stylok <wojciech@stylok.pl>
2019-07-19 22:14:35 +0200

    fake products repository added

---
+++ BricksStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BricksStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product {Name="Kombajn kołowy", Price=780},
+            new Product {Name="Wesołe miasteczko", Price=1200},
+            new Product {Name="Taj Mahal", Price=1700}
+        }.AsQueryable<Product>();
+    }
+}
