69bb182e0afc1b8f7ca1a1952d289bd71279822d
Wojciech Stylok <wojciech@stylok.pl>
2019-07-24 22:21:43 +0200

    pagination unit test

--- BricksStore.Tests/ProductControllerTests.cs
+++ BricksStore.Tests/ProductControllerTests.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using BricksStore.Models;
+using BricksStore.Controllers;
 using Xunit;
 
 namespace BricksStore.Tests
@@ -8,7 +13,28 @@ namespace BricksStore.Tests
         [Fact]
         public void Can_Paginate()
         {
+            //arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product{ProductId=1,Name="P1"},
+                new Product{ProductId=2,Name="P2"},
+                new Product{ProductId=3,Name="P3"},
+                new Product{ProductId=4,Name="P4"},
+                new Product{ProductId=5,Name="P5"}
+            }).AsQueryable<Product>());
 
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //act
+            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+
+            //assert
+            Product[] prodArray = result.ToArray();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P4", prodArray[0].Name);
+            Assert.Equal("P5", prodArray[1].Name);
         }
     }
 }
